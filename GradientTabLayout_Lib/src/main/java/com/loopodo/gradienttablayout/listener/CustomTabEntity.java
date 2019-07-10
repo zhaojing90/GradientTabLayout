d06f112064ed6a6dd27ec8c8c867fa8086951c0b
zhaojing90 <877156635@qq.com>
2019-07-10 11:51:50 +0800

    first commit

---
+++ GradientTabLayout_Lib/src/main/java/com/loopodo/gradienttablayout/listener/CustomTabEntity.java
@@ -0,0 +1,13 @@
+package com.loopodo.gradienttablayout.listener;
+
+import android.support.annotation.DrawableRes;
+
+public interface CustomTabEntity {
+    String getTabTitle();
+
+    @DrawableRes
+    int getTabSelectedIcon();
+
+    @DrawableRes
+    int getTabUnselectedIcon();
+}
---
+++ GradientTabLayout_Lib/src/main/java/com/loopodo/gradienttablayout/listener/OnTabSelectListener.java
@@ -0,0 +1,6 @@
+package com.loopodo.gradienttablayout.listener;
+
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
